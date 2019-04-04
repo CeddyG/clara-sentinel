3eb42ba32e16860fb4221f7f6ca98d0c85856e81
CeddyG <ceddyg@free.fr>
2019-04-04 14:03:20 +0200

    -Set the right event

---
+++ src/Events/User/ReminderEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Events\User;
+
+use Cartalyst\Sentinel\Users\EloquentUser;
+use Cartalyst\Sentinel\Reminders\EloquentReminder;
+
+/**
+ * Description of ReminderEvent
+ *
+ * @author Cedric
+ */
+class ReminderEvent
+{
+    public $oUser;
+    public $oReminder;
+
+    public function __construct(EloquentUser $oUser, EloquentReminder $oReminder)
+    {
+        $this->oUser     = $oUser;
+        $this->oReminder = $oReminder;
+    }
+}
